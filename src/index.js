ef4fa672f9829faa5bae938a232de9e8a83e99f1
VivianaS14 <vasorianp@gmail.com>
2022-12-16 23:03:32 -0500

    feat: Agregando estilos

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
 import './styles/style.scss';
-import bgStar from './assets/images/bg-stars.svg';
-
-console.log("Hello world");
-
-const app = document.getElementById('app');
-const h1 = document.createElement('h1');
-h1.innerText = "Hello beautiful world!!~";
-
-app.appendChild(h1);
-
-const img = document.createElement('img');
-img.src = bgStar;
-img.setAttribute('alt', 'Bg-star');
-app.appendChild(img);
