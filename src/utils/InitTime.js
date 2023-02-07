40171321a048e587db532f38e574c962a4055773
VivianaS14 <vasorianp@gmail.com>
2023-02-07 12:21:36 -0500

    Update Date

--- src/utils/InitTime.js
+++ src/utils/InitTime.js
@@ -2,19 +2,19 @@ import CountTime from "./CountTime";
 import SetTimeLeft from "./SetTimeLeft";
 
 let timeLeft = {
-    d: 0,
-    h: 0,
-    m: 0,
-    s: 0,
-}
+  d: 0,
+  h: 0,
+  m: 0,
+  s: 0,
+};
 
 const InitTime = (_totalSeconds) => {
-    _totalSeconds = Math.floor((new Date('01.14.2023') - new Date()) / 1000);
-    SetTimeLeft(_totalSeconds);
-    let interval = setInterval(() => {
-        if (_totalSeconds < 0) clearInterval(interval);
-        CountTime(_totalSeconds);
-    }, 1000);
-}
+  _totalSeconds = Math.floor((new Date("01.14.2024") - new Date()) / 1000);
+  SetTimeLeft(_totalSeconds);
+  let interval = setInterval(() => {
+    if (_totalSeconds < 0) clearInterval(interval);
+    CountTime(_totalSeconds);
+  }, 1000);
+};
 
 export { InitTime, timeLeft };
